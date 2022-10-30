7dc5d59eb3cdc24e1f4851db9714ba6f8439f819
princia <princia23mendonca@gmail.com>
2022-10-30 14:53:57 +0400

    create routes

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,11 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
+    Route::post('/add-item/{product}', 'App\Http\Controllers\OrderController@store');
+    Route::get('/cart', 'App\Http\Controllers\OrderController@index');
+    Route::get('/paid-orders', 'App\Http\Controllers\OrderController@paidOrders');
+    Route::post('/process-payment', 'App\Http\Controllers\PaymentController@processPayment');
+    Route::delete('/order-item/{orderItemID}', 'App\Http\Controllers\OrderController@deleteOrderItem');
+    Route::apiResource('/product', 'App\Http\Controllers\ProductController');
 });
